4dd5293255005f607ee5a840e676c7baa417a21e
Xinyun Tang <xt222@cornell.edu>
2018-05-16 00:13:03 -0400

    try auto merge

--- pie.js
+++ pie.js
@@ -4,7 +4,6 @@ var oldValue = []; // previous values of the sliders
 colors = {"rect1": "#E2C843", "rect2": "#85BB4B", "rect3": "#F5BAC7"}
 console.log(colors.rect1)
 // pie chart radius
-var radius = 200;
 
 // setup the margins so we don't clip the outter labels
 var margin = {
@@ -17,10 +16,10 @@ var margin = {
 width = 800;
 height = 300;
 
-var svg = d3.select("#slider");
+var svg = d3.select("#id");
 var slider = svg.append("g")
     .attr("class", "slider")
-    .attr("transform", "translate(" + margin.left + "," + 0 + ")");
+    .attr("transform", "translate(" + 10 + "," + 0 + ")");
 
 var x = d3.scaleLinear()
     .domain([0, 100])
@@ -32,7 +31,7 @@ var rect1 = slider.append("rect")
     .attr("class", "track")
     // .attr("x", 20)
     // .attr("y", 150)
-    .attr("height", 240)
+    .attr("height", 140)
     .attr("width", 20)
     .attr("fill", "pink")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
@@ -57,10 +56,20 @@ var handle1 = slider.append("rect", ".track-overlay")
     .attr("id", "handle1")
     .attr("fill", "black")
     .attr("x", -2)
-    .attr("y", 80)
+    .attr("y", 40)
     .attr("class", "handle")
-    // .attr("cy")
-    .attr("r", 9);
+    .attr("r", 9)
+    .call( d3.drag()
+    .on("start.interrupt", function() { 
+      console.log("interrupt is called");
+      hue(d3.event.y) 
+      slider.interrupt(); 
+    })
+    .on("start drag", function() { 
+      console.log("drag is called");
+      hue(d3.event.y); 
+    })
+    );
 
 console.log(handle1)
 
@@ -70,9 +79,20 @@ var handle2 = slider.append("rect", ".track-overlay")
   .attr("id", "handle2")
   .attr("fill", "black")
   .attr("x", -2)
-  .attr("y", 160)
+  .attr("y", 100)
   .attr("class", "handle")
-  .attr("r", 9);
+  .attr("r", 9)
+  .call( d3.drag()
+  .on("start.interrupt", function() { 
+    console.log("interrupt is called");
+    hue(d3.event.y) 
+    slider.interrupt(); 
+  })
+  .on("start drag", function() { 
+    console.log("drag is called");
+    hue(d3.event.y); 
+  })
+  );
 
 slider.transition() // Gratuitous intro!
     .duration(750)
@@ -83,22 +103,25 @@ slider.transition() // Gratuitous intro!
     })
 
 function hue(h) {
-  var handle1_pos = parseInt(d3.select("#handle1").attr("y"));
-  var handle2_pos = parseInt(d3.select("#handle2").attr("y"));
-  var handle1_diff =  Math.abs(h - parseInt(d3.select("#handle1").attr("y")));
-  var handle2_diff =  Math.abs(h - parseInt(d3.select("#handle2").attr("y")));
-
-  var ratio1 = handle1_pos/250;
-  var ratio2 = handle2_pos/250 - ratio1;
-  var ratio3 = 1 - ratio1 - ratio2;
-
-  console.log(ratio1, ratio2, ratio3)
-  if (handle1_diff < 20) {
-    move_handle1(h, handle1_pos, handle2_pos);
+  if (h <= 140 && h > 0) {
+    var handle1_pos = parseInt(d3.select("#handle1").attr("y"));
+    var handle2_pos = parseInt(d3.select("#handle2").attr("y"));
+    var handle1_diff =  Math.abs(h - parseInt(d3.select("#handle1").attr("y")));
+    var handle2_diff =  Math.abs(h - parseInt(d3.select("#handle2").attr("y")));
+  
+    var ratio1 = handle1_pos/150;
+    var ratio2 = handle2_pos/150 - ratio1;
+    var ratio3 = 1 - ratio1 - ratio2;
+  
+    console.log(ratio1, ratio2, ratio3)
+    if (handle1_diff < 10) {
+      move_handle1(h, handle1_pos, handle2_pos);
+    }
+    else if (handle2_diff < 10) {
+      move_handle2(h, handle1_pos, handle2_pos);
+    }
   }
-  else if (handle2_diff < 20) {
-    move_handle2(h, handle1_pos, handle2_pos);
-  }
+  
   // svg.style("background-color", d3.hsl(h, 0.8, 0.8));
 }
 
@@ -109,7 +132,7 @@ function move_handle1(h, pos1, pos2) {
   d3.select("#rect1").remove();
   slider.append("rect")
   .attr("id", "rect1")
-  .attr("width", 22)
+  .attr("width", 20)
   .attr("height", h)
   .attr("fill", colors.rect1)
   .call( d3.drag()
@@ -127,7 +150,7 @@ function move_handle1(h, pos1, pos2) {
   d3.select("#rect2").remove();
   slider.append("rect")
   .attr("id", "rect2")
-  .attr("width", 22)
+  .attr("width", 20)
   .attr("height", pos2 - pos1 - 10)
   .attr("y", parseInt(pos1) + 10)
   .attr("fill", colors.rect2)
@@ -149,8 +172,8 @@ function move_handle2(h, pos1, pos2) {
   d3.select("#rect3").remove();
   slider.append("rect")
   .attr("id", "rect3")
-  .attr("width", 22)
-  .attr("height", 250-h)
+  .attr("width", 20)
+  .attr("height", 130-h)
   .attr("y", h + 10)
   .attr("fill", colors.rect3)
   .call( d3.drag()
@@ -168,7 +191,7 @@ function move_handle2(h, pos1, pos2) {
   d3.select("#rect2").remove();
   slider.append("rect")
   .attr("id", "rect2")
-  .attr("width", 22)
+  .attr("width", 20)
   .attr("height", h - pos1 - 10)
   .attr("y", parseInt(pos1) + 10)
   .attr("fill", colors.rect2)
--- script.js
+++ script.js
@@ -9,103 +9,171 @@ var year_slider = d3.slider().min(1996).max(2018).ticks(0).stepValues(d3.range(1
 d3.select("#year_slider").call(year_slider);
 d3.select("#year_value").text(2000);
 
+$('#cityInput').autocomplete({
+  // autoFocus: true,
+  source: 
+  // function(request, response) {
+  //     var results = $.ui.autocomplete.filter(cities, request.term);
+  //     response(results.slice(0,10)); // only display five auto suggestions
+  // }
+      function(req, responseFn) {
+        var re = $.ui.autocomplete.escapeRegex(req.term);
+        // var matcher = new RegExp( "^" + re, "i" );
+        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex(request.term), "i" );
+        response( $.grep( cities, function( item ){
+          return matcher.test( item.label );
+      }) );
+      //   response( select.children( "option" ).map(function() {
+      //     var text = $( this ).text();
+      //     if ( this.value && ( !request.term || matcher.test(text) ) )
+      //         return {
+      //             label: text.replace(
+      //                 new RegExp(
+      //                     "(?![^&;]+;)(?!<[^<>]*)(" +
+      //                     $.ui.autocomplete.escapeRegex(request.term) +
+      //                     ")(?![^<>]*>)(?![^&;]+;)", "gi"
+      //                 ), "<strong>$1</strong>" ),
+      //             value: text,
+      //             option: this
+      //         };
+      // }) );
+    }
+  }).keyup(function() { // restrict the input to the available trails in the data
+    var isValid = false;
+    for (i in cities) {
+        if (cities[i].toLowerCase().match(this.value.toLowerCase())) {
+            isValid = true;
+        }
+    }   
+    if (!isValid) {
+        this.value = defaltValue;
+    } else {
+        defaltValue = this.value;
+    }
+  });
+
+$('#stockInput').autocomplete({
+  // autoFocus: true,
+  source: function(request, response) {
+      var results = $.ui.autocomplete.filter(stocks, request.term);
+      response(results.slice(0,10)); // only display five auto suggestions
+  }
+  }).keyup(function() { // restrict the input to the available trails in the data
+    var isValid = false;
+    for (i in stocks) {
+        if (stocks[i].toLowerCase().match(this.value.toLowerCase())) {
+            isValid = true;
+        }
+    }   
+    if (!isValid) {
+        this.value = defaltValue;
+    } else {
+        defaltValue = this.value;
+    }
+  });
+
+
+
+
+
 // autocomplete part comes from https://www.w3schools.com/howto/howto_js_autocomplete.asp
-function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
-  var currentFocus;
-  /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
-      }
-  });
-  /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
-      }
-  });
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
-    }
-  }
-}
+// function autocomplete(inp, arr) {
+//   /*the autocomplete function takes two arguments,
+//   the text field element and an array of possible autocompleted values:*/
+//   var currentFocus;
+//   /*execute a function when someone writes in the text field:*/
+//   inp.addEventListener("input", function(e) {
+//       var a, b, i, val = this.value;
+//       /*close any already open lists of autocompleted values*/
+//       closeAllLists();
+//       if (!val) { return false;}
+//       currentFocus = -1;
+//       /*create a DIV element that will contain the items (values):*/
+//       a = document.createElement("DIV");
+//       a.setAttribute("id", this.id + "autocomplete-list");
+//       a.setAttribute("class", "autocomplete-items");
+//       /*append the DIV element as a child of the autocomplete container:*/
+//       this.parentNode.appendChild(a);
+//       /*for each item in the array...*/
+//       for (i = 0; i < arr.length; i++) {
+//         /*check if the item starts with the same letters as the text field value:*/
+//         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+//           /*create a DIV element for each matching element:*/
+//           b = document.createElement("DIV");
+//           /*make the matching letters bold:*/
+//           b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+//           b.innerHTML += arr[i].substr(val.length);
+//           /*insert a input field that will hold the current array item's value:*/
+//           b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+//           /*execute a function when someone clicks on the item value (DIV element):*/
+//               b.addEventListener("click", function(e) {
+//               /*insert the value for the autocomplete text field:*/
+//               inp.value = this.getElementsByTagName("input")[0].value;
+//               /*close the list of autocompleted values,
+//               (or any other open lists of autocompleted values:*/
+//               closeAllLists();
+//           });
+//           a.appendChild(b);
+//         }
+//       }
+//   });
+//   /*execute a function presses a key on the keyboard:*/
+//   inp.addEventListener("keydown", function(e) {
+//       var x = document.getElementById(this.id + "autocomplete-list");
+//       if (x) x = x.getElementsByTagName("div");
+//       if (e.keyCode == 40) {
+//         /*If the arrow DOWN key is pressed,
+//         increase the currentFocus variable:*/
+//         currentFocus++;
+//         /*and and make the current item more visible:*/
+//         addActive(x);
+//       } else if (e.keyCode == 38) { //up
+//         /*If the arrow UP key is pressed,
+//         decrease the currentFocus variable:*/
+//         currentFocus--;
+//         /*and and make the current item more visible:*/
+//         addActive(x);
+//       } else if (e.keyCode == 13) {
+//         /*If the ENTER key is pressed, prevent the form from being submitted,*/
+//         e.preventDefault();
+//         if (currentFocus > -1) {
+//           /*and simulate a click on the "active" item:*/
+//           if (x) x[currentFocus].click();
+//         }
+//       }
+//   });
+//   function addActive(x) {
+//     /*a function to classify an item as "active":*/
+//     if (!x) return false;
+//     /*start by removing the "active" class on all items:*/
+//     removeActive(x);
+//     if (currentFocus >= x.length) currentFocus = 0;
+//     if (currentFocus < 0) currentFocus = (x.length - 1);
+//     /*add class "autocomplete-active":*/
+//     x[currentFocus].classList.add("autocomplete-active");
+//   }
+//   function removeActive(x) {
+//     /*a function to remove the "active" class from all autocomplete items:*/
+//     for (var i = 0; i < x.length; i++) {
+//       x[i].classList.remove("autocomplete-active");
+//     }
+//   }
+//   function closeAllLists(elmnt) {
+//     /*close all autocomplete lists in the document,
+//     except the one passed as an argument:*/
+//     var x = document.getElementsByClassName("autocomplete-items");
+//     for (var i = 0; i < x.length; i++) {
+//       if (elmnt != x[i] && elmnt != inp) {
+//       x[i].parentNode.removeChild(x[i]);
+//     }
+//   }
+// }
+
 /*execute a function when someone clicks in the document:*/
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
-}
+// }
 
 var cities = ['New York, NY', 'Los Angeles, CA', 'Chicago, IL', 'Houston, TX', 'Philadelphia, PA', 'Phoenix, AZ', 'Las Vegas, NV',
        'San Antonio, TX', 'San Diego, CA', 'San Jose, CA', 'Jacksonville, FL', 'San Francisco, CA', 'Austin, TX',

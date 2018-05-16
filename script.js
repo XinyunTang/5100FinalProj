ed576bc0427983b7f2d9b997ad21353f4e821e54
st883 <>
2018-05-16 17:00:28 -0400

    update report 1st

--- script.js
+++ script.js
@@ -214,3 +214,60 @@ $("#amount").val("$" + $("#slider-range").slider("values", 0) +
     " - $" + $("#slider-range").slider("values", 1));
 
 
+var data = [    {"area": "central ", "value": 18000},
+    {"area": "Riverside ", "value": 17000},
+    {"area": "Picton ", "value": 80000},
+    {"area": "Everton ", "value": 55000},
+    {"area": "Kensington ", "value": 100000},
+    {"area": "Kirkdale", "value": 50000}];
+
+//sort bars based on value
+data = data.sort(function (a, b) {
+    return d3.ascending(a.value, b.value);
+})
+
+var margin = {top: 20, right: 20, bottom: 20, left: 20};
+var width = 600 - margin.left - margin.right,
+    height = 230 - margin.top - margin.bottom;
+
+var svg = d3.select("#bar-left").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+  
+var x = d3.scaleLinear().range([0, width]);
+var y = d3.scaleBand().range([height, 0]);
+
+var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+data.sort(function(a, b) { return a.value - b.value; });
+  
+x.domain([0, d3.max(data, function(d) { return d.value; })]);
+y.domain(data.map(function(d) { return d.area; })).padding(0.1);
+
+g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(5).tickFormat(function(d) { return parseInt(d / 1000); }).tickSizeInner([-height]));
+
+g.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(y));
+
+g.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", 0)
+    .attr("height", y.bandwidth())
+    .attr("y", function(d) { return y(d.area); })
+    .attr("width", function(d) { return x(d.value); })
+    .on("mousemove", function(d){
+        tooltip
+          .style("left", d3.event.pageX - 50 + "px")
+          .style("top", d3.event.pageY - 70 + "px")
+          .style("display", "inline-block")
+          .html((d.area) + "<br>" + "£" + (d.value));
+    });
+
+
